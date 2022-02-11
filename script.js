a0d92fbcaceccb8364a38cd5b6ea16cf44ba2e73
Nicole <67175550+Nicole1707@users.noreply.github.com>
2022-02-11 15:08:54 -0800

    JS in process

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const quoteText = document.querySelector(".quote"),
+authorName = document.querySelector(".author .name"),
 quoteBtn = document.querySelector("button");
 
 
 function randomQuote(){
-    
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading quote..."
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
+        console.log(result);
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.innerText = "New quote..."
+    })
 }
-quoteBtn.addEventListener("click", randomQuote)
+quoteBtn.addEventListener("click", randomQuote);
